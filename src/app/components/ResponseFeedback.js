eed1851ea1f7d54a0d2712cdc83ae18c0c7efa3c
Guilherme Kipper <33471042+guikipper@users.noreply.github.com>
2024-04-17 22:47:52 +0000

    adicionado mensagem de retorno ao cliente quando solicita envio de novo link de confirmação de email

--- src/app/components/ResponseFeedback.js
+++ src/app/components/ResponseFeedback.js
@@ -1,9 +1,16 @@
 import styles from '../styles/ResponseFeeback.module.css'
 
-export function ResponseFeedback({message, type}) {
+export function ResponseFeedback({message, type, details}) {
     return (
-        <div className={styles.responseContainer}>
-            <p className={styles.type}>{message}</p>
+        <div className={`${styles.responseContainer} ${type === 'success' ? styles.successDiv : styles.errorDiv}`}>
+            <ul>
+            <li className={`${type === 'success' ? styles.success : styles.error}`}>{message}</li>
+            { details.length > 1 && (
+                <li className={`${type === 'success' ? styles.success : styles.error}`}>{details}</li>
+            )}
+            </ul>
+            
+            
         </div>
     )
 }
--- src/app/resendEmailVerification/page.js
+++ src/app/resendEmailVerification/page.js
@@ -4,23 +4,30 @@ import { useState } from "react";
 import styles from "../styles/ResendEmailVerification.module.css";
 import { resendEmailVerification } from "../api/services/apiFunctions";
 import { ResponseFeedback } from "../components/ResponseFeedback";
+import Loading from "../components/Loading";
 
 export default function RecoverPassword() {
   const [email, setEmail] = useState("");
   const [message, setMessage] = useState("")
   const [type, setType] = useState("")
+  const [details, setDetails] = useState(" ")
+  const [loading, setLoading] = useState(true)
 
   const callApi = async () => {
+    setLoading(true)
     if (email) {
       const response = await resendEmailVerification(email);
       console.log(response)
       if (response.success) {
-        console.log("success");
+        setLoading(false)
+        setType("success")
         setMessage(response.success.message)
       }
       if (response.error) {
-        console.log("error");
+        setLoading(false)
+        setType("error")
         setMessage(response.error.message)
+        setDetails(response.error.details[0].message)
       }
     }
   };
@@ -55,7 +62,15 @@ export default function RecoverPassword() {
           Enviar
         </button>
       </div>
-      <ResponseFeedback type={type} message={message}/>
+      <ResponseFeedback type={type} message={message} details={details}/>
+
+      <div className={styles.loadingWrapper}>
+                    {loading && (
+                        <div className={styles.loadingImage}>
+                            <Loading/>
+                        </div>
+                    )}
+                </div>
     </div>
   );
 }
